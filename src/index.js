15493f810a4daabbefed8ee9ab75d33a855a4185
Chibuzo Ekwue <chieezy5@gmail.com>
2022-11-03 17:36:28 +0100

    added the browser and helmet provider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<HelmetProvider>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</HelmetProvider>
+	</React.StrictMode>
 );
